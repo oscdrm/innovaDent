d39ad9c2c9a79a02556cc6709b7f973c532ccf4b
oscardaRM <gotiel.orm@gmail.com>
2020-05-09 12:01:20 -0500

    fix consultas

--- resources/views/consults/create.blade.php
+++ resources/views/consults/create.blade.php
@@ -92,14 +92,14 @@
                     </div>
                 </div>
 
-                @if(auth()->user()->role->id == 1)
+               
                     <div class="form-group" id="dc">
                                     <label class="font-normal">Selecciona la fecha de consulta:</label>
                                     <div class="input-group date dc-date">
                                         <span class="input-group-addon"><i class="fa fa-calendar"></i></span><input id="date-consult" name="date-consult" type="text" data-date-format="dd/mm/yyyy" class="form-control dci">
                                     </div>
                     </div>
-                @endif
+               
 
 
                 <div class="hr-line-dashed"></div>
