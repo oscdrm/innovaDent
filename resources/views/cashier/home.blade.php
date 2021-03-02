10515ac22aef4cbca0570aa0ea13ba5bc7da8150
Oscar Daniel Rangel Martinez <oscar.rangel@lunave.com>
2021-03-02 09:58:19 -0600

    cambio colores

--- resources/views/cashier/home.blade.php
+++ resources/views/cashier/home.blade.php
@@ -6,7 +6,7 @@
     <div class="col-lg-12">
         <div class="col-lg-2">
            <a href="{{url('/patients/create')}}">
-                <div class="widget darkblue1-bg p-lg text-center">
+                <div class="widget lightblue1-bg  p-lg text-center">
                     <div class="m-b-md">
                         <i style="margin-bottom:5px;" class="fa fa-user-o fa-4x"></i>
                         <h3 class="font-bold no-margins">
@@ -19,7 +19,7 @@
 
         <div class="col-lg-2">
             <a href="{{url('/consults/create')}}">
-                <div class="widget darkblue2-bg p-lg text-center">
+                <div class="widget blue3-bg p-lg text-center">
                     <div class="m-b-md">
                         <i style="margin-bottom:5px;" class="fa fa-bolt fa-4x"></i>
                         <h3 class="font-bold no-margins">
@@ -32,7 +32,7 @@
 
         <div class="col-lg-2">
            <a href="{{url('/cashier/cash')}}">
-                <div class="widget darkblue4-bg p-lg text-center">
+                <div class="widget darkblue1-bg p-lg text-center">
                     <div class="m-b-md">
                         <i style="margin-bottom:5px;" class="fa fa-money fa-4x"></i>
                         <h3 class="font-bold no-margins">
@@ -46,7 +46,7 @@
 
         <div class="col-lg-2">
             <a>
-                <div class="widget darkblue3-bg p-lg text-center">
+                <div class="widget darkblue2-bg p-lg text-center">
                     <div class="m-b-md">
                         <i style="margin-bottom:5px;" class="fa fa-print fa-4x"></i>
                         <h3 class="font-bold no-margins">
@@ -60,7 +60,7 @@
 
         <div class="col-lg-2">
            <a href="" target="_blank" id="agenda">
-                <div class="widget aqua-bg p-lg text-center">
+                <div class="widget darkblue4-bg p-lg text-center">
                     <div class="m-b-md">
                         <i style="margin-bottom:5px;" class="fa fa-book fa-4x"></i>
                         <h3 class="font-bold no-margins">
@@ -73,7 +73,7 @@
 
         <div class="col-lg-2">
             <a href="" target="_blank" id="correo">
-                <div class="widget blue3-bg p-lg text-center">
+                <div class="widget darkblue3-bg p-lg text-center">
                     <div class="m-b-md">
                         <i style="margin-bottom:5px;" class="fa fa-envelope fa-4x"></i>
                         <h3 class="font-bold no-margins">
