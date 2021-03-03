b910928eb790b257cd50e6fd7e29e12d4b80646d
Oscar Daniel Rangel Martinez <oscar.rangel@lunave.com>
2021-03-03 09:49:04 -0600

    edit servicio0

--- resources/views/concepts/edit.blade.php
+++ resources/views/concepts/edit.blade.php
@@ -1,86 +1,90 @@
 @extends('layouts.app2')
 @section('content')
 
-<div class="row wrapper border-bottom white-bg page-heading shad">
-                <div class="col-lg-9">
-                    <h2>Servicios</h2>
-                    <ol class="breadcrumb">
-                        <li>
-                            <a href="{{url('/home')}}">Home</a>
-                        </li>
-                        <li>
-                            <a href="{{url('/concepts')}}">Servicios</a>
-                        </li>
-                        <li class="active">
-                            <strong>Editar Servicio</strong>
-                        </li>
-                    </ol>
-                </div>
-</div><!-- END SECCION TITULO-->
+    <div class="row wrapper border-bottom white-bg page-heading">
+        <div class="col-lg-9">
+            <h2>Rahex</h2>
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item">
+                    <a href="{{url('/concepto')}}">Concepto</a>
+                </li>
+                <li class="breadcrumb-item active">
+                    <strong>Editar concepto</strong>
+                </li>
+            </ol>
+        </div>
+    </div>
 
 <div class="wrapper wrapper-content animated fadeInRight">
 
         <div class="row">
 
-        <div class="ibox-content shad">
-            <h4>Editar servicio</h4>
-            <div class="hr-line-dashed"></div>
-            @if($errors->any())
-                <div class="form-group">
-                        @foreach($errors->all() as $error)
-                            <p class="alert label-danger">{{$error}}</p>
-                        @endforeach
-                </div>
-            @endif
-            <form method="post" action="{{url('/concepts/'.$concept->id.'/edit')}}" class="form-horizontal" enctype="multipart/form-data">
-                @csrf
-                <div class="form-group">
-                    <label class="col-sm-2 control-label">Nombre</label>
-                    <div class="col-sm-10"><input name="name" type="text" class="form-control" value="{{old('name', $concept->name)}}"></div>
-                </div>
-
-                <div class="form-group">
-                        <label class="col-sm-2 control-label">Tienda</label>
-                        <div class="col-sm-10">
-                            <select data-placeholder="Selecciona Tags de la noticia" id="surgeries" name="surgeries[]" class="chosen-select" multiple  tabindex="2" required>
-                                <option value="">Selecciona una tienda</option>
-                                @foreach ($stores as $store)
-                                    <option value="{{$store->id}}">{{$store->name}}</option>
-                                
-                                    @if(count($conceptsSurgery) > 0)
-                                        @foreach ($conceptsSurgery as $conceptSurgery)
-                                            @php
-                                                if($store->id == $conceptSurgery->id){
-                                                    $selected = "selected";
-                                                    echo '<option '.$selected.' value="'.$store->id.'">'.$store->name.'</option>';
-                                                }
-                                            @endphp
-                                        @endforeach
-                                    @endif
-                                @endforeach
-                            </select>
-                        </div>
-                </div>
-
-               
-
+            <div class="ibox-content shad col-12">
+                <h4>Editar concepto</h4>
                 <div class="hr-line-dashed"></div>
-                <div class="form-group">
-                    <div class="col-sm-4 col-sm-offset-9">
-                        <a class="btn btn-white" href="{{url('/concepts')}}">Cancelar</a>
-                        <button class="btn btn-primary" type="submit">Guardar</button>
+                @if($errors->any())
+                    <div class="form-group">
+                            @foreach($errors->all() as $error)
+                                <p class="alert label-danger">{{$error}}</p>
+                            @endforeach
                     </div>
+                @endif
+                <div class="ibox-content">
+                    <form method="post" action="{{url('/concepts/'.$concept->id.'/edit')}}" class="form-horizontal" enctype="multipart/form-data">
+                        @csrf
+                
+                         <div class="form-group">
+                            <label class="col-sm-2 control-label">Nombre</label>
+                            <div class="col-sm-10"><input name="name" type="text" class="form-control" value="{{old('name', $concept->name)}}"></div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="col-sm-2 control-label">Costo</label>
+                            <div class="col-sm-10"><input name="amount" type="number" class="form-control" value="{{old('amount', $concept->amount)}}"></div>
+                        </div>
+
+                        <div class="form-group">
+                                <label class="col-sm-2 control-label">Tienda</label>
+                                <div class="col-sm-10">
+                                    <select data-placeholder="Selecciona una tienda" name="surgeries[]" id="tiendas" class="chosen-select" multiple  tabindex="2">
+                                        <option value="">Selecciona una tienda</option>
+                                        @foreach ($stores as $store)
+                                            <option value="{{$store->id}}">{{$store->name}}</option>
+                                        
+                                            @if(count($conceptsSurgery) > 0)
+                                                @foreach ($conceptsSurgery as $conceptSurgery)
+                                                    @php
+                                                        if($store->id == $conceptSurgery->id){
+                                                            $selected = "selected";
+                                                            echo '<option '.$selected.' value="'.$store->id.'">'.$store->name.'</option>';
+                                                        }
+                                                    @endphp
+                                                @endforeach
+                                            @endif
+                                        @endforeach
+                                    </select>
+                                </div>
+                        </div>
+
+
+                        <br>
+                        <div class="hr-line-dashed"></div>
+                        <div class="form-group">
+                            <div class="col-sm-4 col-sm-offset-9">
+                                <a class="btn btn-white" href="{{url('/concepts')}}">Cancelar</a>
+                                <button class="btn btn-primary" id="save" type="submit">Guardar</button>
+                            </div>
+                        </div>
+                        
+
+                    </form>
                 </div>
 
-            
-            </form>
-
-        </div>
+            </div>
 
 
         </div><!--END CONTAINER ROW-->
+
+        
 </div><!--END WRAPER-->
-
-@include('layouts/disablebutton')
-
 @endsection
