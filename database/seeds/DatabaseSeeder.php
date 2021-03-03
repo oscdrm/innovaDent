242ef7b0f955c89ae581983ac3364e8305c8a071
Oscar Daniel Rangel Martinez <oscar.rangel@lunave.com>
2021-03-03 01:06:04 -0600

    seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(RolesSeeder::class);
-        $this->call(SurgerySeeder::class);
-        $this->call(UsersTableSeeder::class);
+        //$this->call(RolesSeeder::class);
+        //$this->call(SurgerySeeder::class);
+        //$this->call(UsersTableSeeder::class);
+        $this->call(MetodoPagoSeeder::class);
     }
 }
