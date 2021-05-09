eb656c9cc1249529e79441b2edaf2e04d74bcdc2
Oscar Daniel  Rangel Martinez <oscdrm@MacBook-Pro-de-Oscar.local>
2021-05-08 21:25:24 -0500

    cambio monto semana

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -89,7 +89,7 @@
         <div class="col-lg-3">
             <div class="widget darkblue4-bg p-lg text-center shad">
                     <div class="m-b-md">
-                        <i class="fa fa-dollar fa-4x"></i> <span style="font-size:55px;">{{$amountTodayHuetamo}}</span>
+                        <i class="fa fa-dollar fa-4x"></i> <span style="font-size:55px;">{{$amountWeekHuetamo}}</span>
                         <h3 class="font-bold no-margins">
                             Corte de la semana Huetamo
                         </h3>
